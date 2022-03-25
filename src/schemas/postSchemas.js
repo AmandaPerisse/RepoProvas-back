6eebc60530490b01c583eb0c96d5fa60da5a4f46
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-03-24 23:33:20 -0300

    fix: post schema

--- src/schemas/postSchemas.js
+++ src/schemas/postSchemas.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const postSchema = joi.object({
     url: joi.string().uri().required(),
-    description: joi.string()
+    description: joi.string().allow(null, '')
 });
 
 export default postSchema;
