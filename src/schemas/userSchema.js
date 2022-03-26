d0776d830864130766241fdb21d3a47963f2837e
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-03-26 15:57:41 -0300

    make it better

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,7 +1,7 @@
 import joi from 'joi';
 
 const userSchema = joi.object({
-  userName: joi.string().required(),
+  name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
   pictureUrl: joi.string().uri().required()
