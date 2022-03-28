8d8419d96216fb3d767bb6a6e194bc5f1a96c761
Amanda <amandaveras1999@gmail.com>
2022-03-28 11:05:04 -0300

    fix: conflict fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import userRouter from "./userRouter.js";
+import postRouter from "./postRouter.js";
 import trendingHashtagsRouter from "./trendingHashtagsRouter.js";
-
 
 const router = Router();
 router.use(authRouter);
 router.use(userRouter);
+router.use(postRouter);
 router.use(trendingHashtagsRouter);
-
 
 export default router;
