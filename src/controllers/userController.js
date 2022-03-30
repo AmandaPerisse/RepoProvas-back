3f6713b567ed8706c2bda1dbadcc3366b7b8c31c
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-03-30 19:31:45 -0300

    refactor: userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,33 +1,31 @@
 import bcrypt from 'bcrypt';
 import { connection } from '../database.js';
+import { getExistingUser, createUser } from '../repositories/userRepository.js';
 
 export async function postUser(req, res) {
   const user = req.body;
 
   try {
-    const isThereUser = await connection.query('SELECT * FROM users WHERE email=$1', [user.email])
-    if (isThereUser.rowCount !== 0) {
+    const isThereUser = await getExistingUser(user.email);
+    if (isThereUser)
       return res.sendStatus(409);
-    }
 
-    const passwordHash = bcrypt.hashSync(user.password, 10);
-    await connection.query(`
-      INSERT INTO 
-        users ("name", email, password, "pictureUrl") 
-      VALUES ($1, $2, $3, $4)
-    `, [user.name, user.email, passwordHash, user.pictureUrl])
+    await createUser(user.name, user.email, user.password, user.pictureUrl);
 
     res.sendStatus(201);
+
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
   }
 }
+
 export async function getUser(req, res) {
   const { user } = res.locals;
 
   try {
     res.send(user);
+    
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,7 +1,9 @@
 import { connection } from '../database.js';
+import bcrypt from 'bcrypt';
 
 export async function getExistingUser(email) {
-	return await connection.query('SELECT * FROM users WHERE email=$1', [email]);
+	const getUserQuery = await connection.query('SELECT * FROM users WHERE email=$1', [email]);
+	return getUserQuery.rows[0];
 }
 
 export async function createNewSession(token, userId) {
@@ -10,4 +12,13 @@ export async function createNewSession(token, userId) {
 
 export async function createUserObject(token, userName, email, pictureUrl) {
 	return { token, user: { userName, email, pictureUrl } };
+}
+
+export async function createUser(name, email, password, pictureUrl) {
+	const passwordHash = bcrypt.hashSync(password, 10);
+    return await connection.query(`
+      INSERT INTO 
+        users ("name", email, password, "pictureUrl") 
+      VALUES ($1, $2, $3, $4)
+    `, [name, email, passwordHash, pictureUrl])
 }
