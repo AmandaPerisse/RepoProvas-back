205080e7f2657c1993522e729ad512031af3bfa1
gbmahou1 <gbmahou1@hotmail.com>
2022-03-30 17:43:21 -0300

    firstVersion

---
+++ src/controllers/followersController.js
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import { connection } from '../database.js';
+
+export async function unfollow (req, res) {
+    const { userId, followerId } = req.params;
+    try 
+    {
+        if (!userId || !followerId)
+        {
+            return res.sendStatus(400)
+        }
+        await connection.query(`DELETE FROM followers WHERE "userId" = $1 AND "friendId" = $2`, [userId, followerId]);
+    } 
+    catch (error) 
+    {
+        alert('Não foi possível realizar a operação...');
+        console.log(error);
+        return res.sendStatus(500);
+    }
+}
+
+export async function follow (req, res) {
+    const { userId, followerId } = req.params;
+    try
+    {
+        if (!userId || !followerId)
+        {
+            return res.sendStatus(400)
+        }
+        await connection.query(`
+        INSERT INTO followers ("userId", "friendId")
+        VALUES ($1, $2)
+        `, [userId, followerId]);
+    }
+    catch (error) 
+    {
+        alert('Não foi possível realizar a operação...');
+        console.log(error);
+        return res.sendStatus(500);
+    }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import authRouter from "./authRouter.js";
 import userRouter from "./userRouter.js";
 import postRouter from "./postRouter.js";
+import followerRouter from "./followersRouter.js";
 import trendingHashtagsRouter from "./trendingHashtagsRouter.js";
 
 const router = Router();
 router.use(authRouter);
 router.use(userRouter);
 router.use(postRouter);
+router.use(followerRouter)
 router.use(trendingHashtagsRouter);
 
 export default router;
